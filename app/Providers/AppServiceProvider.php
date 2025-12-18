1091f915422b4033135e739292c8db184f465f1b
Sajjad Naqvi <sajjadmehdi1214@gmail.com>
2025-12-18 22:23:20 +0500

    WIP! working on chat module

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Response::macro('success', function ($data, int $code = Response::HTTP_OK) {
+            return response()->json([
+                'success' => true,
+                'data' => $data,
+            ], $code);
+        });
     }
 }
